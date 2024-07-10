dfa2784ec3120189d8282c7d3b98f5a8f06044ec
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-07-10 18:01:42 +0500

    1. Two Sum Problem Solved JAVA

---
+++ 1. Two Sum/App.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+public class App {
+
+    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        App app = new App();
+
+        System.out.println("Enter Size of Nums: ");
+        int size = app.scanner.nextInt();
+
+        int[] nums = createArray(size);
+
+        System.out.println("Enter Target: ");
+        int target = app.scanner.nextInt();
+
+        TwoSumSolution solution = new TwoSumSolution(size, nums, target);
+        int[] output = solution.TwoSum();
+        printArray(output);
+    }
+
+    public static int[] createArray(int size) {
+        App app = new App();
+        int[] nums = new int[size];
+
+        System.out.println("Enter Array");
+        for (int i = 0; i < size; i++) {
+            nums[i] = app.scanner.nextInt();
+        }
+        return nums;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ 1. Two Sum/TwoSumSolution.java
@@ -0,0 +1,27 @@
+
+public class TwoSumSolution {
+
+    int[] nums ;
+    int target;
+
+    public TwoSumSolution(int size, int[] nums, int target) {
+        this.nums = nums;
+        this.target = target;
+    }
+
+    public int[] TwoSum() {
+        int[] output = new int[2];
+        int temp;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                temp = nums[i] + nums[j];
+                if (temp == target) {
+                    output[0] = i;
+                    output[1] = j;
+                    return output;
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
