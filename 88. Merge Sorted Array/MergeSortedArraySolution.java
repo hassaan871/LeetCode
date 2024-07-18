f1310cbfa491bfc9577490245f848759ae7ab483
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-07-18 18:45:56 +0500

    88. Merge Sorted Array updated code by using best programming practices

--- 88. Merge Sorted Array/MergeSortedArraySolution.java
+++ 88. Merge Sorted Array/MergeSortedArraySolution.java
@@ -1,42 +1,55 @@
 import java.util.Scanner;
 
 public class MergeSortedArraySolution {
-    int m,n;
-    int[] nums1;
-    int[] nums2;
-    int[] arr;
+    private int m,n;
+    private int[] nums1;
+    private int[] nums2;
+    private int[] mergedArray;
 
 
     public MergeSortedArraySolution(){
         arrayInput();
-        arr = mergeSortedArray(nums1, nums2);
-        arrayPrint(arr);
+        mergedArray = mergeSortedArray(nums1, nums2);
+        arrayPrint(mergedArray);
     }
 
-    public void arrayInput(){
+    /**
+     * Takes input for two arrays nums1 and nums2 from the user.
+     */
+    private void arrayInput(){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter m: ");
-        m = scanner.nextInt();
+        try {
+            System.out.println("Enter m: ");
+            m = scanner.nextInt();
 
-        System.out.println("Enter n: ");
-        n = scanner.nextInt();
+            System.out.println("Enter n: ");
+            n = scanner.nextInt();
 
-        nums1 = new int[m];
-        nums2 = new int[n];
+            nums1 = new int[m];
+            nums2 = new int[n];
 
-        System.out.println("Enter nums1 : ");
-        for(int i=0; i<m; i++){
-            nums1[i] =  scanner.nextInt();
-        }
+            System.out.println("Enter nums1 : ");
+            for(int i=0; i<m; i++){
+                nums1[i] =  scanner.nextInt();
+            }
 
-        System.out.println("Enter nums2: ");
-        for(int i=0; i<n; i++){
-            nums2[i] = scanner.nextInt();
+            System.out.println("Enter nums2: ");
+            for(int i=0; i<n; i++){
+                nums2[i] = scanner.nextInt();
+            }
+        } finally {
+            scanner.close();
         }
     }
 
-    public int[] arraySort(int[] arr){
+    /**
+     * Sort array in ascending order
+     * 
+     * @param arr Array to be sorted
+     * @return A soted array
+     */
+    private int[] arraySort(int[] arr){
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
                 int temp = arr[i];
@@ -50,8 +63,14 @@ public class MergeSortedArraySolution {
         }
         return arr;
     }
-
-    public int[] mergeSortedArray(int[] nums1, int[] nums2){
+    /**
+     * Merge two arrays into single sorted array
+     * 
+     * @param nums1 First array
+     * @param nums2 Second array
+     * @return A merged and sorted array
+     */
+    private int[] mergeSortedArray(int[] nums1, int[] nums2){
         int[] arr = new int[nums1.length + nums2.length];
         int index = 0;
         for(int i=0; i<nums1.length; i++){
@@ -64,7 +83,12 @@ public class MergeSortedArraySolution {
         return arraySort(arr);
     }
 
-    public void  arrayPrint(int[] arr){
+    /**
+     * Prints the elements of the given array.
+     * 
+     * @param arr The array to print
+     */
+    private void  arrayPrint(int[] arr){
         for(int i=0; i<arr.length; i++){
             System.out.println(arr[i]);
         }
